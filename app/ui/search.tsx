86b40d72443867453ce06334c16e428d8c71ab8d
tariq-rana <tariq_rana@yahoo.com>
2026-01-08 07:53:07 +0700

    Chapter 10

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,14 +3,19 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter ();
 
-  function handleSearch(term: string){
+  const handleSearch = useDebouncedCallback((term: string) => {
+  
     const params = new URLSearchParams(searchParams);
+    
+    params.set('page','1');
+
     if(term){
       params.set('query', term);
     }else{
@@ -18,7 +23,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathName}?${params.toString()}`);
-  }
+  },300);
+  
   
   return (
     <div className="relative flex flex-1 flex-shrink-0">
